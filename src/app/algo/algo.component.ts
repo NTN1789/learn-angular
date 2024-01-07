acc378665826630ac7cb21c7875a844f57b3b0cd
Natan <natanalmeida040@gmail.com>
2024-01-07 13:09:22 -0300

    parei aula 12 components e ciclo de vida

--- src/app/algo/algo.component.ts
+++ src/app/algo/algo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-algo',
@@ -7,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './algo.component.html',
   styleUrl: './algo.component.scss'
 })
-export class AlgoComponent  implements OnInit	 {
+export class AlgoComponent  implements OnInit, OnChanges	 {
+ @Input() public title:string = "Bem vindo Zé";
+
+
   constructor() { }
+  ngOnChanges(): void {
+    // input serve para receber dados de outro component
+    console.log("alterado com sucesso ")
+    console.log(this.title) // ola mundo vai retornar
+  }
 
 
   ngOnInit(): void {
+    // ciclo de vida do angular
 
   }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,   } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { AlgoComponent } from './algo/algo.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [CommonModule, RouterOutlet, AlgoComponent],
+  template: `
+  <app-algo title="ola mundo"></app-algo>
+  <router-outlet></router-outlet>`,
 
 })
-export class AppComponent {
-  public title = "Bem-vindo"
+export class AppComponent  implements OnInit {
+
+    constructor(){
+
+    }
+
+    ngOnInit() :void{
+
+    }
 }
