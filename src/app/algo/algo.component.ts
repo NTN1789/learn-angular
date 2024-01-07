5018943776acab1021dbc91a598a0df7c88e1062
Natan <natanalmeida040@gmail.com>
2024-01-07 11:54:34 -0300

    first commit

---
+++ src/app/algo/algo.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-algo',
+  standalone: true,
+  imports: [],
+  templateUrl: './algo.component.html',
+  styleUrl: './algo.component.scss'
+})
+export class AlgoComponent  implements OnInit	 {
+  constructor() { }
+
+
+  ngOnInit(): void {
+
+  }
+
+}
